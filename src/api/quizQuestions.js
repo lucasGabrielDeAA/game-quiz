04214a018ab94a86c5ec41ac01753229a32179a5
lucasGabrielDeAA <lg.technus@gmail.com>
2018-03-06 16:50:08 -0300

    API directory added

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,89 @@
+var quizQuestions = [
+    {
+        question: "What franchise would you rather play a game from?",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "Gears of War"
+            },
+            {
+                type: "Nintendo",
+                content: "Zelda"
+            },
+            {
+                type: "Sony",
+                content: "The Last of Us"
+            }
+        ]
+    },
+    {
+        question: "Which console would you prefer to play with friends?",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "X-Box One"
+            },
+            {
+                type: "Nintendo",
+                content: "Nintendo Switch"
+            },
+            {
+                type: "Sony",
+                content: "Playstation 4"
+            }
+        ]
+    },
+    {
+        question: "Which of these racing franchises would you prefer to play a game from?",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "Forza"
+            },
+            {
+                type: "Nintendo",
+                content: "Mario Kart"
+            },
+            {
+                type: "Sony",
+                content: "Gran Turismo"
+            }
+        ]
+    },
+    {
+        question: "Which of these games do you think is best?",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "BioShock"
+            },
+            {
+                type: "Nintendo",
+                content: "The Legend of Zelda: Breath of the Wild"
+            },
+            {
+                type: "Sony",
+                content: "God of War III"
+            }
+        ]
+    },
+    {
+        question: "What console would you prefer to own?",
+        answers: [
+            {
+                type: "Microsoft",
+                content: "X-Box One"
+            },
+            {
+                type: "Nintendo",
+                content: "Nintento Switch"
+            },
+            {
+                type: "Sony",
+                content: "Playstation 4"
+            }
+        ]
+    }
+  ];
+  
+  export default quizQuestions;
